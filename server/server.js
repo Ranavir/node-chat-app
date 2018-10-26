79f90a4fdbe678f297efdc4191a21b6d1ee58d88
CISCO\randash <randash@cisco.com>
2018-10-27 05:24:01 +0530

    Greet, new users and alert others

--- server/server.js
+++ server/server.js
@@ -15,15 +15,33 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) =>{
   console.log('New User connected');
 
+  socket.emit('newMessage',{
+    from : 'Admin',
+    message: 'Welcome to the chat App',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from : 'Admin',
+    message: 'New User Joined',
+    createdAt: new Date().getTime()
+  });
 
   socket.on('createMessage', (message)=>{
     console.log('createMessage Event :',message);
-    io.emit('newMessage', {
-      from : message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    // io.emit('newMessage', {
+    //   from : message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from : message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
+
+
   socket.on('disconnect', ()=>{
     console.log('Disconnected from client');
   });
