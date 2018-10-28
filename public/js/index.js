15a5bef73e66bbf554858854169f3af30af877df
CISCO\randash <randash@cisco.com>
2018-10-28 06:39:52 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -18,12 +18,44 @@ socket.on('disconnect', function(){
   console.log('Disconnected from server');
 });
 
+socket.on('newLocationMessage',function(message){
+  var li = $('<li></li>');
+  var a = $('<a target="_blank">My current location</a>');
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+  $('#messages').append(li);
+});
+
+
 $('#message-from').on('submit',function(e){
   e.preventDefault();
+  var messageTextbox = $('[name=message]');
   socket.emit('createMessage',{
     from : 'user',
-    text : $('[name=message]').val()
+    text : messageTextbox.val()
   },function(){
-
+    messageTextbox.val('');
   });
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click',function(){
+  if(!navigator.geolocation){
+    return alert('Geolocation is not supported by your browser.');
+  }
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
+
+  navigator.geolocation.getCurrentPosition(function(postion){
+    locationButton.removeAttr('disabled').text('Send location');
+    // console.log(postion);
+    socket.emit('createLocationMessage',{
+      latitude : postion.coords.latitude,
+      longitude : postion.coords.longitude
+    });
+  },function(){
+    locationButton.removeAttr('disabled').text('Send location');
+    alert('Unable to fetch location');
+  });
+
 });
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 console.log(publicPath);//C:\ranvir\Projects\NodeProjects\node-chat-app\public
 
@@ -23,15 +23,12 @@ io.on('connection',(socket) =>{
   socket.on('createMessage', (message, callback)=>{
     console.log('createMessage Event :',message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // callback();
     callback('This is message from server.');
-    // socket.broadcast.emit('newMessage', {
-    //   from : message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
   });
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude, coords.longitude));
+  });
 
   socket.on('disconnect', ()=>{
     console.log('Disconnected from client');
